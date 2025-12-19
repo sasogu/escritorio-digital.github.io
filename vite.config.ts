3f14fcc7e203c4de07bf63bfa731bfa667931965
Juan Jose de Haro <jjdeharo@gmail.com>
2025-12-19 13:04:31 +0100

    fix(pages): adjust base path and update ca strings

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,31 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Escritorio Digital',
-        short_name: 'Escritorio',
-        description: 'Un escritorio digital interactivo con herramientas y widgets para el aula.',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: 'escritorio-digital.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'escritorio-digital.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    })
-  ], 
-})
+export default defineConfig(({ command }) => ({
+    base: command === 'build' ? '/escritorio/' : '/',
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Escritorio Digital',
+                short_name: 'Escritorio',
+                description: 'Un escritorio digital interactivo con herramientas y widgets para el aula.',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'escritorio-digital.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'escritorio-digital.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        })
+    ],
+}))
